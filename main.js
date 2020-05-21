5eb36dacc3948583bc980599cd1ac2b446560586
Edoardo Piergentili <epiergentili@outlook.it>
2020-05-21 15:27:36 +0200

    start

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	//Code
+  var source   = $("#template").html();
+  var template = Handlebars.compile(source);
+  var container = $(".cds-container");
+
+  $.ajax({
+    "url" : "https://flynn.boolean.careers/exercises/api/array/music" ,
+    "method" : "GET" ,
+    "success" : function (data) {
+      var disk = data.response;
+      console.log(disk);
+      for (var i = 0; i < disk.length; i++) {
+       var disk_corrente = disk[i];
+       console.log(disk_corrente);
+       var html = template(disk_corrente);
+       container.append(html);
+
+
+      }
+
+    },
+    "error" : function () {
+      alert("IL SITO NON RISPONDE");
+    }
+  });
 });
