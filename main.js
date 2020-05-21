2b367c8d9f7d7736ad146ef268bdf5bd8b45f3d2
Edoardo Piergentili <epiergentili@outlook.it>
2020-05-21 17:19:16 +0200

    FINE

--- main.js
+++ main.js
@@ -2,22 +2,50 @@ $(document).ready(function() {
   var source   = $("#template").html();
   var template = Handlebars.compile(source);
   var container = $(".cds-container");
+  var select = $('select');
+
+  var disco = {
+    titolo : "" ,
+    img : "" ,
+    autore : "",
+    anno : "",
+    genere : ""
+  };
+
+  select.on('change', function() {
+    var value_select = this.value;
+    console.log(value_select);
+    $(".cd").each(function(){
+      var corrente = $(this)
+      corrente.addClass("disabled")
+      if (corrente.hasClass(value_select)) {
+        corrente.removeClass("disabled")
+      } else if (value_select == "All") {
+        corrente.removeClass("disabled")
+      };
+    });
+  });
 
   $.ajax({
     "url" : "https://flynn.boolean.careers/exercises/api/array/music" ,
     "method" : "GET" ,
     "success" : function (data) {
       var disk = data.response;
-      console.log(disk);
+
       for (var i = 0; i < disk.length; i++) {
+
        var disk_corrente = disk[i];
-       console.log(disk_corrente);
-       var html = template(disk_corrente);
+
+       disco.titolo = disk_corrente.title;
+       disco.img = disk_corrente.poster;
+       disco.autore = disk_corrente.author;
+       disco.anno = disk_corrente.year;
+       disco.genere = disk_corrente.genre;
+
+       var html = template(disco);
        container.append(html);
 
-
       }
-
     },
     "error" : function () {
       alert("IL SITO NON RISPONDE");
